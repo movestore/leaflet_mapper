c769510a752bc54d86346569a6ba3dfa53b1011e
ricschuster <mail@richard-schuster.com>
2020-07-16 16:40:06 -0700

    initial commit

---
+++ ShinyModule.R
@@ -0,0 +1,61 @@
+library(move)
+library(shiny)
+library(raster)
+library(foreach)
+library(sf)
+library(fasterize)
+library(rgeos)
+library(leaflet)
+
+# setwd("/root/app/")
+
+shinyModuleUserInterface <- function(id, label, grid = 50000) {
+  ns <- NS(id)
+  
+  tagList(
+    titlePanel("Basic leaflet map"),
+    leafletOutput(ns("leafmap"))
+  )
+}
+
+shinyModuleConfiguration <- function(id, input) {
+  ns <- NS(id)
+  configuration <- list()
+  
+  print(ns('grid'))
+  
+  configuration["grid"] <- input[[ns('grid')]]
+  
+  configuration
+}
+
+shinyModule <- function(input, output, session, data, grid = 50000) {
+  dataObj <- reactive({ data })
+  current <- reactiveVal(data)
+  
+  output$leafmap <- renderLeaflet({
+    bounds <- as.vector(bbox(extent(dataObj())))
+    outl <- leaflet() %>% 
+      fitBounds(bounds[1], bounds[2], bounds[3], bounds[4]) %>% 
+      addTiles() %>%
+      addProviderTiles("Esri.WorldStreetMap",group = "StreetMap") %>%
+      addProviderTiles("Esri.WorldImagery", group = "Aerial") %>%
+      addProviderTiles("Stamen.Terrain", group = "Terrain") %>%
+      addPolylines(data =  as(dataObj(),'SpatialLines'), color ="grey", group = "lines") %>%
+      addCircles(data = dataObj(), fillOpacity = 0.3, opacity = 0.5, color="blue", group = "points") %>%
+      addLegend(position= "topright", colors=c("grey","blue"), 
+                labels=c("lines","points") ,opacity = 0.7, title = "data") %>%
+      addScaleBar(position="bottomleft", 
+                  options=scaleBarOptions(maxWidth = 100, 
+                                          metric = TRUE, imperial = F, updateWhenIdle = TRUE)) %>%
+      addLayersControl(
+        baseGroups = c("StreetMap", "Aerial", "Terrain"),
+        overlayGroups = c("lines", "points"),
+        options = layersControlOptions(collapsed = FALSE)
+      )
+        
+    outl    
+  })  
+  
+  return(reactive({ current() }))
+}
